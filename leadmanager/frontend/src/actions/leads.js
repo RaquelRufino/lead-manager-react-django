aa75973d25fb81ebbca8a06e5233c5631519ef0c
RaquelRufino <quelrufino35@gmail.com>
2019-11-07 20:51:11 -0300

    add Error Handling and Alerts

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
-
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
+import { createMessage } from './messages';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 
 
 
 export const getLeads = () => dispatch => {
 
     axios
-    .get('/api/leads/')
+        .get('/api/leads/')
         .then(res => {
             dispatch({
                 type: GET_LEADS,
@@ -20,8 +20,9 @@ export const getLeads = () => dispatch => {
 export const deleteLead = (id) => dispatch => {
 
     axios
-    .delete(`/api/leads/${id}/`)
+        .delete(`/api/leads/${id}/`)
         .then(res => {
+            dispatch(createMessage({ deleteLead: "Lead Deleted" }));
             dispatch({
                 type: DELETE_LEAD,
                 payload: id
@@ -33,12 +34,22 @@ export const deleteLead = (id) => dispatch => {
 export const addLead = (lead) => dispatch => {
 
     axios
-    .post('/api/leads/', lead)
+        .post('/api/leads/', lead)
         .then(res => {
+            dispatch(createMessage({ addLead: "Lead Added" }));
             dispatch({
                 type: ADD_LEAD,
                 payload: res.data
             });
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            const errors = {
+                msg: err.response.data,
+                status: err.response.status
+            }
+            dispatch({
+                type: GET_ERRORS,
+                payload: errors
+            });
+        });
 };
--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import leads from './leads';
+import errors from './erros';
+import messages from "./messages";
 
 export default combineReducers({
-    leads
+    leads,
+    errors,
+    messages
 });
